aa4321e62cbfaca7cd5a0bc1554357ae794888d0
KeepBitcoinFree-org <jigsawcrew@protonmail.com>
2019-11-23 01:11:16 -0500

    cleaning up the code some

--- index.js
+++ index.js
@@ -7,60 +7,51 @@ var port = process.env.PORT || 80;
 let BITBOX = require('bitbox-sdk').BITBOX;
 let bitbox = new BITBOX();
 //let mnemonic = bitbox.Mnemonic.generate();
-// couple sleep cruise hybrid physical nature spin hedgehog put paddle silver laundry
-
 //console.log('Mnemonic: ', mnemonic);
-
-// require syntax
-
-
-
-
-
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-//connected
+// new user connected
 
-	 socket.emit('chat message', 'Hello friend. This is a Node.js app utilzing the BITBOX npm module. Enter any BCH address to have it converted from/to Legacy/CashAddr format and get other details about the address.');
+  // welcome message to user on this specific socket
+	socket.emit('chat message', 'Hello friend! This is a Node.js app utilzing socket.io & the BITBOX npm module. Enter a BCH address in any format to display all information about the address.');
 
-
-	//submitted chat message
+	// fucntion for submitted form
   socket.on('chat message', function(msg){
-  	// message (msg) has been submitted, take action
+    // message (msg) has been submitted, take actions
     
-    // io.emit('chat message', msg);
-
+    // trim the message string to remove any leading or late 
     msg = msg.trim();
 
+    // async function to get all details of BCH address
     (async () => {
   try {
+    //use BITBOX to get details of BCH address submitted by user
     let details = await bitbox.Address.details(msg);
 
-    //break down details array & print to user
-
-//	var arrayLength = details.length;
-//	for (var i = 0; i < arrayLength; i++) {
+    // break down details array & print to user
     socket.emit('chat message', 'Balance: ' + details.balance + ' BCH');
     socket.emit('chat message', 'Balance in Sats: ' + details.balanceSat);
     socket.emit('chat message', 'Unconfirmed Balance: ' + details.unconfirmedBalance + ' BCH');
     socket.emit('chat message', 'Unconfirmed Balance in Sats: ' + details.unconfirmedBalanceSat);
     socket.emit('chat message', 'Total Received: ' + details.totalReceived + ' BCH');
     socket.emit('chat message', 'Total Sats Received: ' + details.totalReceivedSat);
-	socket.emit('chat message', 'Total Sats Sent: ' + details.totalSentSat);
+	  socket.emit('chat message', 'Total Sats Sent: ' + details.totalSentSat);
    
 	while (details.unconfirmedBalance > 0){
 			socket.emit('chat message', 'Unconfirmed balance detected.... Tracking the balance until it is confirmed.')
 
+      //re-check the balance every 5 seconds until balance == 0
 			await new Promise(resolve => setTimeout(resolve, 5000));
 
 			// set interval to run clearFunc every 12 hours in milliseconds
 			let details = await bitbox.Address.details(msg);
 			socket.emit('chat message', 'Unconfirmed Balance: ' + details.unconfirmedBalance + ' BCH');
 
+      // check if balance is 0
 			if (details.unconfirmedBalance == 0){
 				socket.emit('chat message', 'Unconfirmed Balance for ' + msg + ' has been confirmed!');
 				window.alert('Unconfirmed Balance has been confirmed!');
@@ -69,14 +60,11 @@ io.on('connection', function(socket){
 
 		}
 
-
-
+    //separator for each address
     socket.emit('chat message', '₿');
-    //Do something
-//	}
-
     
   } catch(error) {
+  // catch error for details array and print only to console if it fails
    console.error(error)
   }
 })()
@@ -128,24 +116,10 @@ io.on('connection', function(socket){
 			return;
     	}
 
-
-    // get details about an address using async function & bitbox
-    // 1BFHGm4HzqgXXyNX8n7DsQno5DAC4iLMRA
-
-
   });
 });
 
-// https://bchjs.cash/
-
-
-// app.post('/', async function (req, res) {
-
-// 	let address = req.m;
-//	console.log('BCH address:', address);
-
-// });
-
+// Listen on port set in ENV file, otherwise use default port 80
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
