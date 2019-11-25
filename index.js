1e822310c31ceceb989299e8837819f55c42c7d7
KeepBitcoinFree-org <jigsawcrew@protonmail.com>
2019-11-25 00:18:16 -0500

    adding more functionality using BITBOX and some visual enhancements, examples, etc.

--- index.js
+++ index.js
@@ -30,12 +30,12 @@ io.on('connection', function(socket){
   socket.emit('logo', '_▀█___█▀___▀██████▀__████████▀____██████████_▄█████████▀___▀██████▀__████_______███▄');
   socket.emit('logo', '_________░_____░_░_____░_______░__░_░__________░_░___░____░________░_░__________░_░_');
   socket.emit('logo', '_____________________░___________________░___________░_░__________░_____░___________');
-  socket.emit('logo', '▄_•▄▄▄▄_▄▄▄_.▄▄▄·____▄▄▄▄·▪▄▄▄▄▄▄▄·_____▪__▐_▄_____·▄▄▄▄▄_▄▄▄_▄▄▄_.__________▄▄▄__▄▄_•_');
-  socket.emit('logo', '█▌▄▌▀▄.▀▀▄.▀▐█_▄█____▐█_▀██•██_▐█_▌▪____██•█▌▐█____▐▄▄▀▄_█▀▄.▀▀▄.▀·_____▪____▀▄_█▐█_▀_▪');
-	socket.emit('logo', '▐▀▀▄▐▀▀▪▐▀▀▪▄██▀·____▐█▀▀█▐█▐█.██_▄▄▄█▀▄▐█▐█▐▐▌____██▪▐▀▀▄▐▀▀▪▐▀▀▪▄______▄█▀▄▐▀▀▄▄█_▀█▄');
-  socket.emit('logo', '▐█.█▐█▄▄▐█▄▄▐█▪·•____██▄▪▐▐█▐█▌▐███▐█▌.▐▐███▐█▌____██▌▐█•█▐█▄▄▐█▄▄▌_____▐█▌.▐▐█•█▐█▄▪▐█');
-  socket.emit('logo', '·▀__▀▀▀▀_▀▀▀.▀_______·▀▀▀▀▀▀▀▀▀·▀▀▀_▀█▄▀▀▀▀▀_█▪____▀▀▀.▀__▀▀▀▀_▀▀▀___▀___▀█▄▀.▀__·▀▀▀▀_');
-  socket.emit('logo', '_______________________________________________________________________________________');
+  socket.emit('logo', '▄_•▄▄▄▄_▄▄▄_.▄▄▄·__▄▄▄▄·▪▄▄▄▄▄▄▄·_____▪__▐_▄____·▄▄▄▄▄_▄▄▄_▄▄▄_.__________▄▄▄__▄▄_•_');
+  socket.emit('logo', '█▌▄▌▀▄.▀▀▄.▀▐█_▄█__▐█_▀██•██_▐█_▌▪____██•█▌▐█___▐▄▄▀▄_█▀▄.▀▀▄.▀·_____▪____▀▄_█▐█_▀_▪');
+	socket.emit('logo', '▐▀▀▄▐▀▀▪▐▀▀▪▄██▀·__▐█▀▀█▐█▐█.██_▄▄▄█▀▄▐█▐█▐▐▌___██▪▐▀▀▄▐▀▀▪▐▀▀▪▄______▄█▀▄▐▀▀▄▄█_▀█▄');
+  socket.emit('logo', '▐█.█▐█▄▄▐█▄▄▐█▪·•__██▄▪▐▐█▐█▌▐███▐█▌.▐▐███▐█▌___██▌▐█•█▐█▄▄▐█▄▄▌_____▐█▌.▐▐█•█▐█▄▪▐█');
+  socket.emit('logo', '·▀__▀▀▀▀_▀▀▀.▀_____·▀▀▀▀▀▀▀▀▀·▀▀▀_▀█▄▀▀▀▀▀_█▪___▀▀▀.▀__▀▀▀▀_▀▀▀___▀___▀█▄▀.▀__·▀▀▀▀_');
+  socket.emit('logo', '____________________________________________________________________________________');
 
 
 
@@ -65,10 +65,15 @@ io.on('connection', function(socket){
       msgArray[0] = msgArray[0].trim();
       msgArray[1] = msgArray[1].trim();
       msgArray[2] = msgArray[2].trim();
+      if (msgArray.length > 3){
+       msgArray[3] = msgArray[3].trim();
+      }
         console.log(msgArray);
     //  console.log(msgArray[0]);
    //   console.log(msgArray[1]);
     //  console.log(msgArray[2]);
+
+      // check if sign is called
       if (msgArray[0] == 'sign'){
        // console.log('it is SIGN, signing message');
        // console.log(msgArray[1], msgArray[2]);
@@ -81,6 +86,37 @@ io.on('connection', function(socket){
         return;
       }
 
+      // check if verify is called
+      if (msgArray[0] == 'verify'){
+
+        let verifyMessage = bitbox.BitcoinCash.verifyMessage(msgArray[1], msgArray[2], msgArray[3]);
+        
+        if(verifyMessage == true){
+          socket.emit('chat message', 'Message was verified as authentic by the address that signed the message & cryptographic signature provided. ');
+        }else {
+          socket.emit('chat message', 'Message was NOT verified as authentic.')
+        }
+      }
+
+
+      // check if encodeBIP21 is called
+      if (msgArray[0] == 'encodeBIP21'){
+
+        // encodeBIP21 MODULE - to add options to BCH address such as amount, label & message
+        let address = '1C6hRmfzvWst5WA7bFRCVAqHt5gE2g7Qar'
+        let options = {
+          amount: 12.5,
+          label: 'coinbase donation',
+          message: "and ya don't stop",
+        }
+        
+        var BIP21encoded = bitbox.BitcoinCash.encodeBIP21(address, options)
+        // bitcoincash:qpum6dwnqmmysdggrprse8ccjq7ldcrfqgmmtgcmny?amount=12.5&label=coinbase%20donation&message=and%20ya%20don%27t%20stop
+        console.log(BIP21encoded);
+        socket.emit('chat message', BIP21encoded);
+
+      }
+
 
       //TODO: add functionality for toSatoshi(9) by splitting string into array by ( then again by ).
       //var words = str.split('(');
@@ -88,23 +124,77 @@ io.on('connection', function(socket){
       //var word2 = words[1];
       //var words2 = word2.split(')');
       return;
-    }
-  //  msgArray = msg.split('(', ')');
 
+    }
   }catch(error){
     console.log(error);
   }
+// end of sign message module
 
-  msg = msg.toLowerCase();
+
+  msglow = msg.toLowerCase();
 
   // HELP module
   try{
     console.log('checking if msg qualifies for help module');
-    if (msg == 'help'){
-      socket.emit('update', 'Enter a BCH address in Legacy or CashAddr format to display information about that address. If an unconfirmed balance is detected for that address, the system will track it until it is confirmed.');
-      socket.emit('update', 'Enter "getBlockchainInfo" to return info regarding the BCH Blockchain');
+    if (msglow == 'help'){
+      socket.emit('update', 'Enter a BCH address in Legacy or CashAddr format to display the latest info about that address. If an unconfirmed balance is detected for that address, the system will track it until it is confirmed.');
+      socket.emit('update', 'Enter "getBlockchainInfo" to return the latest info regarding the BCH Blockchain');
+      socket.emit('update', 'Enter "getBlockCount" to return the latest number of blocks in the longest Blockchain');
+      socket.emit('update', 'Enter "getMempoolInfo" to return the latest info about the current mempool');
       socket.emit('update', 'Enter "sign, PRIVATEKEYWIF, MESSAGE" to sign a message with a private key.');
+      socket.emit('example', 'sign, KxtpRDUJDiutLaTV8Vuavhb6h7zq9YV9ZKA3dU79PCgYmNVmkkvS, Bitcoin Cash is Bitcoin');
+      socket.emit('update', 'Enter "verify, BCH_ADDRESS, SIGNATURE, MESSAGE" to verify a signed message.');
+      socket.emit('example', 'verify, bitcoincash:qp2zvw3zpk5xx43w4tve7mtekd9kaxwj4uenq9eupv, IIYVhlo2Z6TWFjYX1+YM+7vQKz0m+zYdSe4eYpFLuAQDEZXqll7lZC8Au22VI2LLP5x+IerZckVk3QQPsA3e8/8=, Bitcoin is Cash');
       socket.emit('update', 'Enter "clear" to clear the screen and start fresh');
+      return;
+    }
+  }catch(error){
+    console.log(error);
+  }
+
+  // GET MEMPOOL INFO
+try{
+      console.log('checking if msg qualifies for getMempoolInfo module');
+    if (msglow == 'getmempoolinfo'){
+      
+  (async () => {
+  try {
+    let getMempoolInfo = await bitbox.Blockchain.getMempoolInfo();
+    console.log(getMempoolInfo);
+   // print entire array at once: socket.emit('array', JSON.stringify(getMempoolInfo));
+    socket.emit('chat message', 'Size: ' + getMempoolInfo.size);
+    socket.emit('chat message', 'Bytes: ' + getMempoolInfo.bytes);
+    socket.emit('chat message', 'Usage: ' + getMempoolInfo.usage);
+    socket.emit('chat message', 'Max Mempool: ' + getMempoolInfo.maxmempool);
+    socket.emit('chat message', 'Mempool Min Fee: ' + getMempoolInfo.mempoolminfee + ' BCH');
+    socket.emit('chat message', 'Min Relay Tx Fee ' + getMempoolInfo.minrelaytxfee + ' BCH');
+
+  } catch(error) {
+   console.error(error)
+  }
+})()
+      return;
+    }
+  }catch(error){
+    console.log(error);
+  }
+
+  // GET BLOCK COUNT module
+
+try{
+      console.log('checking if msg qualifies for getBlockCount module');
+    if (msglow == 'getblockcount'){
+      
+    (async () => {
+  try {
+    let getBlockCount = await bitbox.Blockchain.getBlockCount();
+    console.log(getBlockCount);
+    socket.emit('chat message', 'Block Count: ' + getBlockCount);
+  } catch(error) {
+   console.error(error)
+  }
+})()
       return;
     }
   }catch(error){
@@ -113,14 +203,14 @@ io.on('connection', function(socket){
 
   // GET BLOCKCHAIN INFO module
   try{
-      console.log('checking if msg qualifies for help getBlockchainInfo module');
-    if (msg == 'getblockchaininfo'){
+      console.log('checking if msg qualifies for getBlockchainInfo module');
+    if (msglow == 'getblockchaininfo'){
       
     (async () => {
     try {
       let getBlockchainInfo = await bitbox.Blockchain.getBlockchainInfo();
       //console.log(getBlockchainInfo);
-    socket.emit('update', 'BlockChain Info for BCH:');
+    //socket.emit('update', 'BlockChain Info for BCH:');
     socket.emit('chat message', 'Chain: ' + getBlockchainInfo.chain);
     socket.emit('chat message', 'Blocks: ' + getBlockchainInfo.blocks);
     socket.emit('chat message', 'Best Block Hash: ' + getBlockchainInfo.bestblockhash);
@@ -159,34 +249,26 @@ io.on('connection', function(socket){
       //re-check the balance every 10 seconds until balance == 0
 			await new Promise(resolve => setTimeout(resolve, 10000));
 
-			// set interval to run clearFunc every 12 hours in milliseconds
 			let details = await bitbox.Address.details(msg);
 			socket.emit('chat message', 'Unconfirmed Balance: ' + details.unconfirmedBalance + ' BCH');
 
       // check if balance is 0
 			if (details.unconfirmedBalance == 0){
 				socket.emit('update', 'Unconfirmed Balance for ' + msg + ' has been confirmed!');
-				// window.alert('Unconfirmed Balance has been confirmed!');
+				// TODO: alert user funds have confirmed (needs to be done on client side): window.alert('Unconfirmed Balance has been confirmed!');
 				return;
 			}
 
 		}
 
     //separator for each address
-    socket.emit('chat message', ' ');
+    //socket.emit('chat message', '');
     
   } catch(error) {
   // catch error for details array and print only to console if it fails
    console.error(error)
   }
 })()
-
-	try {
-
-		
-	} catch(error){
-		console.log(error);
-	}
 
     
     // bitcoincash:qzm47qz5ue99y9yl4aca7jnz7dwgdenl85jkfx3znl
