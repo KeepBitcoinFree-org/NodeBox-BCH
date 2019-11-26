ca8d2a5a01637834863c2bfe98e61cff45b0ba1b
KeepBitcoinFree-org <jigsawcrew@protonmail.com>
2019-11-26 01:59:28 -0500

    adding new functionality, updated help

--- index.js
+++ index.js
@@ -38,13 +38,10 @@ io.on('connection', function(socket){
   socket.emit('logo', '____________________________________________________________________________________');
 
   socket.emit('update', ' ');
-
-  socket.emit('update', 'Wake up, Neo...');
-
+  socket.emit('update', 'Wake up, Neo....');
 
   //maybe pause here for a minute, then continue?
-
-  socket.emit('update', 'This is a Node.js app demonstrating some functionality of the Socket.io & BITBOX npm modules. Enter "help" to view all available commands. All data entered is private to your socket, or browser session.');
+  socket.emit('update', 'NodeBox is a Node.js app demonstrating functionality of the Socket.io & BITBOX-sdk npm modules. Enter "help" to view all available commands. All data entered is private to your socket, or browser session.');
 
 	// fucntion for submitted form
   socket.on('chat message', function(msg){
@@ -52,35 +49,76 @@ io.on('connection', function(socket){
     
   // trim the message string to remove any leading or late 
   msg = msg.trim();
-  // .toLowerCase()
-  // root@nodebox.io
+  msglow = msg.toLowerCase();
 
-  socket.emit('chat message', '₿ ' + msg);
+  socket.emit('chat message', '~ ₿ ' + msg);
   //socket.emit('chat message', '₿');
 
   // if they type donate, we handle on client side so return here
-  if (msg.toLowerCase() == 'donate'){
+  if (msglow == 'donate'){
+    // add bitcoincash text donation address. client site returns QR code.
+   socket.emit('update', 'Donate BCH to KeepBitcoinFree.org:');
+   socket.emit('update', 'bitcoincash:qpphskv6mgh62teqjhumsnr6xxqqux9t7cfw9n4rmr');
    return;
   }
- 
-    // USE THIS FOR COMMANDS WITH PARENTHESIS such as toSatoshi(5)
+
+  if (msglow == 'clear'){
+    return;
+  }
+  
+
+  // TRY BLOCK FOR COMMANDS WITH PARENTHESIS such as toSatoshi(5)
+  try{
+
     var regExp = /\(([^)]+)\)/;
-    //var matches = regExp.exec("I expect five hundred dollars ($500).");
-    //console.log(matches[1]);
-    //TODO: add functionality for toSatoshi(9) by splitting string into array by ( then again by ).
-    //var words = str.split('(');
-    //console.log(words);
-    //var word2 = words[1];
-    //var words2 = word2.split(')');
-// }
+    var msgInsideParen = regExp.exec(msglow);
+    //msgInsideParen[1] gives you what is inside the parenthesis
+
+    msgSatArray = msglow.split('(');
+
+  if( msgSatArray.length > 1) {
+    //toSatoshi()
+    if (msgSatArray[0] == 'tosatoshi') {
+      // convert user entered $BCH to satoshis
+      let toSatoshi = bitbox.BitcoinCash.toSatoshi(msgInsideParen[1]);
+      // 9 = 900000000
+      socket.emit('update', msgInsideParen[1] + ' BCH converted to Satoshis: ' + toSatoshi);
+    }
 
 
+    //toBitcoinCash()
+    if (msgSatArray[0] == 'tobitcoincash') {
+        //convert user entered satoshis to $BCH
+        let toBitcoinCash = bitbox.BitcoinCash.toBitcoinCash(msgInsideParen[1]);
+        socket.emit('update', msgInsideParen[1] + ' Satoshis converted to BCH: ' + toBitcoinCash);
+    }
 
+    //utxo(BCH_ADDRESS)
+    if (msgSatArray[0] == 'utxo') {
+        // get all utxos for an address
+        (async () => {
+        try {
+          let utxo = await bitbox.Address.utxo('1M1FYu4zuVaxRPWLZG5CnP8qQrZaqu6c2L');
+          console.log(utxo);
+          socket.emit('update', JSON.stringify(utxo));
 
- // SIGN module: if there are commas present in the msg & the first word is 'sign' then try to sign a message with PrivateKeyWIF
+        } catch(error) {
+          console.error(error)
+         }
+        })()
+    }
+    return;
+  }
+
+  }catch(error){
+    console.log(error);
+  }
+
+  // TRY BLOCK FOR COMMANDS WITH commas , 
+  // SIGN module: if there are commas present in the msg & the first word is 'sign' then try to sign a message with PrivateKeyWIF
   // TESTING: sign, KxtpRDUJDiutLaTV8Vuavhb6h7zq9YV9ZKA3dU79PCgYmNVmkkvS, Bitcoin Cash is Bitcoin
   try {
-      console.log('checking if msg qualifies for the sign message module');
+     // debug: console.log('checking if msg qualifies for the sign message module');
     msgArray = msg.split(',');
   //  console.log('msgArray Length is: ' + msgArray.length);
     if (msgArray.length > 1) {
@@ -92,20 +130,15 @@ io.on('connection', function(socket){
        msgArray[3] = msgArray[3].trim();
       }
         console.log(msgArray);
-    //  console.log(msgArray[0]);
-   //   console.log(msgArray[1]);
-    //  console.log(msgArray[2]);
 
       // check if sign is called
       if (msgArray[0] == 'sign'){
-       // console.log('it is SIGN, signing message');
-       // console.log(msgArray[1], msgArray[2]);
         let signedMessage = bitbox.BitcoinCash.signMessageWithPrivKey(msgArray[1], msgArray[2]);
        // console.log(signedMessage);
-        socket.emit('chat message', 'PrivatekeyWIF: ' + msgArray[1].substring(0, 47) + '*****');
-        socket.emit('chat message', 'Message: ' + msgArray[2]);
-        socket.emit('chat message', 'Signed Message: ' + signedMessage);
-        socket.emit('chat message', ' ');
+        socket.emit('update', 'PrivatekeyWIF: ' + msgArray[1].substring(0, 47) + '*****');
+        socket.emit('update', 'Message: ' + msgArray[2]);
+        socket.emit('update', 'Signed Message: ' + signedMessage);
+        socket.emit('update', ' ');
         return;
       }
 
@@ -115,16 +148,16 @@ io.on('connection', function(socket){
         let verifyMessage = bitbox.BitcoinCash.verifyMessage(msgArray[1], msgArray[2], msgArray[3]);
         
         if(verifyMessage == true){
-          socket.emit('chat message', 'Message was verified as authentic by the address that signed the message & cryptographic signature provided. ');
+          socket.emit('update', 'Message was verified as authentic by the address that signed the message & cryptographic signature provided. ');
         }else {
-          socket.emit('chat message', 'Message was NOT verified as authentic.')
+          socket.emit('update', 'Message was NOT verified as authentic.')
         }
       }
 
 
       // check if encodeBIP21 is called
       if (msgArray[0] == 'encodeBIP21'){
-
+        // TODO: FINISH BIP21encoded
         // encodeBIP21 MODULE - to add options to BCH address such as amount, label & message
         let address = '1C6hRmfzvWst5WA7bFRCVAqHt5gE2g7Qar'
         let options = {
@@ -150,16 +183,21 @@ io.on('connection', function(socket){
 // end of sign message module
 
 
-  msglow = msg.toLowerCase();
+  // TODO: ADD MORE MODUULES
+
 
   // HELP module
   try{
-    console.log('checking if msg qualifies for help module');
+    // debug: console.log('checking if msg qualifies for help module');
     if (msglow == 'help'){
       socket.emit('update', 'Enter a BCH address in Legacy or CashAddr format to display the latest info about that address. If an unconfirmed balance is detected for that address, the system will track it until it is confirmed.');
       socket.emit('update', 'Enter "getBlockchainInfo" to return the latest info regarding the BCH Blockchain');
       socket.emit('update', 'Enter "getBlockCount" to return the latest number of blocks in the longest Blockchain');
+      socket.emit('update', 'Enter "getDifficulty" to return the proof-of-work difficulty as a multiple of the minimum difficulty.');
       socket.emit('update', 'Enter "getMempoolInfo" to return the latest info about the current mempool');
+      socket.emit('update', 'Enter "toSatoshi(# of BCH)" to return the amount of Satoshis for the supplied amount of BCH');
+      socket.emit('update', 'Enter "toBitcoinCash(# of Satoshis)" to return the amount of BCH for supplied amount of Sats');
+      socket.emit('update', 'Enter utxo(BCH_ADDRESS) to return a list of utxos for a legacy or cash address');
       socket.emit('update', 'Enter "sign, PRIVATEKEYWIF, MESSAGE" to sign a message with a private key.');
       socket.emit('example', 'sign, KxtpRDUJDiutLaTV8Vuavhb6h7zq9YV9ZKA3dU79PCgYmNVmkkvS, Bitcoin Cash is Bitcoin');
       socket.emit('update', 'Enter "verify, BCH_ADDRESS, SIGNATURE, MESSAGE" to verify a signed message.');
@@ -174,7 +212,7 @@ io.on('connection', function(socket){
 
   // GET MEMPOOL INFO
 try{
-      console.log('checking if msg qualifies for getMempoolInfo module');
+     // debug: console.log('checking if msg qualifies for getMempoolInfo module');
     if (msglow == 'getmempoolinfo'){
       
   (async () => {
@@ -182,12 +220,12 @@ try{
     let getMempoolInfo = await bitbox.Blockchain.getMempoolInfo();
     console.log(getMempoolInfo);
    // print entire array at once: socket.emit('array', JSON.stringify(getMempoolInfo));
-    socket.emit('chat message', 'Size: ' + getMempoolInfo.size);
-    socket.emit('chat message', 'Bytes: ' + getMempoolInfo.bytes);
-    socket.emit('chat message', 'Usage: ' + getMempoolInfo.usage);
-    socket.emit('chat message', 'Max Mempool: ' + getMempoolInfo.maxmempool);
-    socket.emit('chat message', 'Mempool Min Fee: ' + getMempoolInfo.mempoolminfee + ' BCH');
-    socket.emit('chat message', 'Min Relay Tx Fee ' + getMempoolInfo.minrelaytxfee + ' BCH');
+    socket.emit('update', 'Size: ' + getMempoolInfo.size);
+    socket.emit('update', 'Bytes: ' + getMempoolInfo.bytes);
+    socket.emit('update', 'Usage: ' + getMempoolInfo.usage);
+    socket.emit('update', 'Max Mempool: ' + getMempoolInfo.maxmempool);
+    socket.emit('update', 'Mempool Min Fee: ' + getMempoolInfo.mempoolminfee + ' BCH');
+    socket.emit('update', 'Min Relay Tx Fee ' + getMempoolInfo.minrelaytxfee + ' BCH');
 
   } catch(error) {
    console.error(error)
@@ -202,14 +240,14 @@ try{
   // GET BLOCK COUNT module
 
 try{
-      console.log('checking if msg qualifies for getBlockCount module');
+    // debug:  console.log('checking if msg qualifies for getBlockCount module');
     if (msglow == 'getblockcount'){
       
     (async () => {
   try {
     let getBlockCount = await bitbox.Blockchain.getBlockCount();
     console.log(getBlockCount);
-    socket.emit('chat message', 'Block Count: ' + getBlockCount);
+    socket.emit('update', 'Block Count: ' + getBlockCount);
   } catch(error) {
    console.error(error)
   }
@@ -220,21 +258,50 @@ try{
     console.log(error);
   }
 
+
+  // GET DIFFICULTY module
+  try{
+     // debug: console.log('checking if msg qualifies for getBlockCount module');
+    if (msglow == 'getdifficulty'){
+      
+(async () => {
+  try {
+    let getDifficulty = await bitbox.Blockchain.getDifficulty();
+    // debug: console.log(getDifficulty);
+    socket.emit('update', 'Difficulty: ' + getDifficulty);
+  } catch(error) {
+   console.error(error)
+  }
+})()
+      return;
+    }
+  }catch(error){
+    console.log(error);
+  }
+
+
+
   // GET BLOCKCHAIN INFO module
   try{
-      console.log('checking if msg qualifies for getBlockchainInfo module');
+      // debug: console.log('checking if msg qualifies for getBlockchainInfo module');
     if (msglow == 'getblockchaininfo'){
       
     (async () => {
     try {
-      let getBlockchainInfo = await bitbox.Blockchain.getBlockchainInfo();
-      //console.log(getBlockchainInfo);
+    let getBlockchainInfo = await bitbox.Blockchain.getBlockchainInfo();
+    //console.log(getBlockchainInfo);
     //socket.emit('update', 'BlockChain Info for BCH:');
-    socket.emit('chat message', 'Chain: ' + getBlockchainInfo.chain);
-    socket.emit('chat message', 'Blocks: ' + getBlockchainInfo.blocks);
-    socket.emit('chat message', 'Best Block Hash: ' + getBlockchainInfo.bestblockhash);
-    socket.emit('chat message', 'Difficulty: ' + getBlockchainInfo.difficulty);
-    socket.emit('chat message', 'Mediantime: ' + getBlockchainInfo.mediantime);
+    socket.emit('update', 'Chain: ' + getBlockchainInfo.chain);
+    socket.emit('update', 'Blocks: ' + getBlockchainInfo.blocks);
+    socket.emit('update', 'Headers: ' + getBlockchainInfo.headers);
+    socket.emit('update', 'Best Block Hash: ' + getBlockchainInfo.bestblockhash);
+    socket.emit('update', 'Difficulty: ' + getBlockchainInfo.difficulty);
+    socket.emit('update', 'Mediantime: ' + getBlockchainInfo.mediantime);
+    socket.emit('update', 'Verification Progress: ' + getBlockchainInfo.verificationprogress);
+    socket.emit('update', 'Chainwork: ' + getBlockchainInfo.chainwork);
+    socket.emit('update', 'Pruned: ' + getBlockchainInfo.pruned);
+    socket.emit('update', 'Softfork: ' + JSON.stringify(getBlockchainInfo.softforks));
+    socket.emit('update', 'BIP9_softfork: ' + JSON.stringify(getBlockCount.bip9_softforks));
 
     } catch(error) {
       console.error(error)
@@ -254,13 +321,13 @@ try{
     let details = await bitbox.Address.details(msg);
 
     // break down details array & print to user
-    socket.emit('chat message', 'Balance: ' + details.balance + ' BCH');
-    socket.emit('chat message', 'Balance in Sats: ' + details.balanceSat);
-    socket.emit('chat message', 'Unconfirmed Balance: ' + details.unconfirmedBalance + ' BCH');
-    socket.emit('chat message', 'Unconfirmed Balance in Sats: ' + details.unconfirmedBalanceSat);
-    socket.emit('chat message', 'Total Received: ' + details.totalReceived + ' BCH');
-    socket.emit('chat message', 'Total Sats Received: ' + details.totalReceivedSat);
-	  socket.emit('chat message', 'Total Sats Sent: ' + details.totalSentSat);
+    socket.emit('update', 'Balance: ' + details.balance + ' BCH');
+    socket.emit('update', 'Balance in Sats: ' + details.balanceSat);
+    socket.emit('update', 'Unconfirmed Balance: ' + details.unconfirmedBalance + ' BCH');
+    socket.emit('update', 'Unconfirmed Balance in Sats: ' + details.unconfirmedBalanceSat);
+    socket.emit('update', 'Total Received: ' + details.totalReceived + ' BCH');
+    socket.emit('update', 'Total Sats Received: ' + details.totalReceivedSat);
+	  socket.emit('update', 'Total Sats Sent: ' + details.totalSentSat);
    
 	while (details.unconfirmedBalance > 0){
 			socket.emit('update', 'Unconfirmed balance detected.... Tracking the balance until it is confirmed.')
@@ -269,20 +336,18 @@ try{
 			await new Promise(resolve => setTimeout(resolve, 10000));
 
 			let details = await bitbox.Address.details(msg);
-			socket.emit('chat message', 'Unconfirmed Balance: ' + details.unconfirmedBalance + ' BCH');
+			socket.emit('update', 'Unconfirmed Balance: ' + details.unconfirmedBalance + ' BCH');
 
       // check if balance is 0
 			if (details.unconfirmedBalance == 0){
-				socket.emit('update', 'Unconfirmed Balance for ' + msg + ' has been confirmed!');
+				socket.emit('chat message', 'Unconfirmed Balance for ' + msg + ' has been confirmed!');
 				// TODO: alert user funds have confirmed (needs to be done on client side): window.alert('Unconfirmed Balance has been confirmed!');
 				return;
 			}
 
 		}
-
     //separator for each address
     //socket.emit('chat message', '');
-    
   } catch(error) {
   // catch error for details array and print only to console if it fails
    console.error(error)
@@ -290,42 +355,42 @@ try{
 })()
 
     
-    // bitcoincash:qzm47qz5ue99y9yl4aca7jnz7dwgdenl85jkfx3znl
+  // bitcoincash:qzm47qz5ue99y9yl4aca7jnz7dwgdenl85jkfx3znl
 
-    console.log('********* User entered: ' + msg + ' :deretne resU *********');
+  // debug: console.log('********* User entered: ' + msg + ' :deretne resU *********');
   
   //CONVERT BCH ADDRESS: detects address type & converts to the other, prints both.
   try{
     	if ( bitbox.Address.isLegacyAddress(msg) == true) {
 
     		//print original address
-    		socket.emit('chat message', 'Legacy: ' + msg);
+    		socket.emit('update', 'Legacy: ' + msg);
 
     		// mainnet w/ prefix
-  			msg = bitbox.Address.toCashAddress(msg)
+  			cashAddr = bitbox.Address.toCashAddress(msg);
+        network = bitbox.Address.detectAddressNetwork(msg);
   			// bitcoincash:qzm47qz5ue99y9yl4aca7jnz7dwgdenl85jkfx3znl
-  			socket.emit('chat message', 'CashAddr: ' + msg);
-  			socket.emit('chat message', 'Network: ' + bitbox.Address.detectAddressNetwork(msg));
+  			socket.emit('update', 'CashAddr: ' + cashAddr);
+  			socket.emit('update', 'Network: ' + network);
   			return;
     	}
     	
-    	  	if (bitbox.Address.isCashAddress(msg) == true) {
+    	 if (bitbox.Address.isCashAddress(msg) == true) {
 
     	  	// print original address
-    	  	socket.emit('chat message', 'CashAddress: ' + msg);
+    	   socket.emit('update', 'CashAddress: ' + msg);
 
     		// mainnet w/ prefix
-  			msg = bitbox.Address.toLegacyAddress(msg)
+  			legacy = bitbox.Address.toLegacyAddress(msg);
+        network = bitbox.Address.detectAddressNetwork(msg);
   			// 1HiaTupadqQN66Tvgt7QSE5Wg13BUy25eN
-  			socket.emit('chat message', 'Legacy: ' + msg);
-  			socket.emit('chat message', 'Network: ' + bitbox.Address.detectAddressNetwork(msg));
+  			socket.emit('update', 'Legacy: ' + legacy);
+  			socket.emit('update', 'Network: ' + network);
   			return;
     	}
-
 	}catch(error) {
-
 			console.log(error);
-			socket.emit('chat message', error.message);
+			socket.emit('update', error.message);
 			return;
     	}
 
