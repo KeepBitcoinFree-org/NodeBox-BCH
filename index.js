aa5fac854b8a4933d2a8f2d1dc9d2e79ef2c3a2f
KeepBitcoinFree-org <jigsawcrew@protonmail.com>
2019-11-30 20:56:55 -0500

    had some drinks, saw some things.

--- index.js
+++ index.js
@@ -9,14 +9,33 @@ let bitbox = new BITBOX();
 //let mnemonic = bitbox.Mnemonic.generate();
 //console.log('Mnemonic: ', mnemonic);
 
+// formatting for numbers: if any # requires commas, this does it. thanks to Lloyd Banks
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 
+//TODO: clean this shit up. Now fuck off, I got work to do.
+
+//testing waiting emit
+//socket.emit('waiting', '.');
+
 // NEW USER CONNECTED HERE
 io.on('connection', function(socket){
   
+//#  ┌─┐┌─┐┬_┬┌─┐┬─┐┌─┐┌┬┐__┌┐_┬_┬
+//#  ├─┘│_││││├┤_├┬┘├┤__││__├┴┐└┬┘
+//#  ┴__└─┘└┴┘└─┘┴└─└─┘─┴┘__└─┘_┴_
+
+
+//╔═╗┌─┐┬_┬┌─┐┬─┐┌─┐┌┬┐_╔╗_┬_┬_
+//╠═╝│_││││├┤_├┬┘├┤__││_╠╩╗└┬┘_
+//╩__└─┘└┴┘└─┘┴└─└─┘─┴┘_╚═╝_┴
+
 
   // welcome message to user on this specific socket (each browser visit is a separate socket)
   socket.emit('logo', '███▄▄▄▄____▄██████▄__████████▄_____▄████████_▀█████████▄___▄██████▄__▀████____▐████▀');
@@ -27,14 +46,16 @@ io.on('connection', function(socket){
   socket.emit('logo', '███___███_███____███_███____███___███____█▄____███____██▄_███____███___▐███__▀███___');
   socket.emit('logo', '███___███_███____███_███___▄███___███____███___███____███_███____███__▄███_____███▄_');
   socket.emit('logo', '_▀█___█▀___▀██████▀__████████▀____██████████_▄█████████▀___▀██████▀__████_______███▄');
-  socket.emit('logo', '_░_░_____░_____░_░_____░_______░__░_░__________░_░___░____░________░_░__________░_░_');
-  socket.emit('logo', '_____░_______________░___________________░___________░_░__________░_____░___________');
+  socket.emit('logo', '_░_░_____░_____░_░_____░___╔═╗┌─┐┬_┬┌─┐┬─┐┌─┐┌┬┐_╔╗_┬_┬__░_░___░____░________░_░____');
+  socket.emit('logo', '____░__________░___________╠═╝│_││││├┤_├┬┘├┤__││_╠╩╗└┬┘______░_____________░________');
+  socket.emit('logo', '___________________________╩__└─┘└┴┘└─┘┴└─└─┘─┴┘_╚═╝_┴_________________________░____');
   socket.emit('logo', '▄_•▄▄▄▄_▄▄▄_.▄▄▄·__▄▄▄▄·▪▄▄▄▄▄▄▄·_____▪__▐_▄____·▄▄▄▄▄_▄▄▄_▄▄▄_.__________▄▄▄__▄▄_•_');
   socket.emit('logo', '█▌▄▌▀▄.▀▀▄.▀▐█_▄█__▐█_▀██•██_▐█_▌▪____██•█▌▐█___▐▄▄▀▄_█▀▄.▀▀▄.▀·_____▪____▀▄_█▐█_▀_▪');
 	socket.emit('logo', '▐▀▀▄▐▀▀▪▐▀▀▪▄██▀·__▐█▀▀█▐█▐█.██_▄▄▄█▀▄▐█▐█▐▐▌___██▪▐▀▀▄▐▀▀▪▐▀▀▪▄______▄█▀▄▐▀▀▄▄█_▀█▄');
   socket.emit('logo', '▐█.█▐█▄▄▐█▄▄▐█▪·•__██▄▪▐▐█▐█▌▐███▐█▌.▐▐███▐█▌___██▌▐█•█▐█▄▄▐█▄▄▌_____▐█▌.▐▐█•█▐█▄▪▐█');
   socket.emit('logo', '·▀__▀▀▀▀_▀▀▀.▀_____·▀▀▀▀▀▀▀▀▀·▀▀▀_▀█▄▀▀▀▀▀_█▪___▀▀▀.▀__▀▀▀▀_▀▀▀___▀___▀█▄▀.▀__·▀▀▀▀_');
   socket.emit('logo', '____________________________________________________________________________________');
+
 
   socket.emit('update', ' ');
   
@@ -75,7 +96,7 @@ io.on('connection', function(socket){
   }, 3300);
 
   setTimeout(function() {
-      socket.emit('update', 'Welcome. NodeBox is an interactive terminal powered by the Node.js, Socket.io & BITBOX modules. Using NodeBox, you can view information about Bitcoin Cash addresses or the Blockchain. Enter "help" to view all available commands. All data entered is private to each socket, or browser session.');
+      socket.emit('update', 'Welcome. NodeBox is an interactive terminal utilizing Node.js, Socket.io & the BITBOX-sdk. Using NodeBox, you can view information about specific Bitcoin Cash addresses or the BCH blockchain. Enter "help" to view all available commands. All data entered is private to each socket, or current browser session.');
      // socket.emit('example', 'Nodebox is still in beta. If you find critical any errors, let us know and we may send you a reward in SLP or BCH');
   }, 4400);
   
@@ -100,11 +121,11 @@ io.on('connection', function(socket){
     (async () => {
     try {
      let getMiningInfo = await bitbox.Mining.getMiningInfo();
-     console.log(getMiningInfo);
+     //console.log(getMiningInfo);
      socket.emit('update', 'Blocks: ' + getMiningInfo.blocks);
      socket.emit('update', 'CurrentBlocksize: ' + getMiningInfo.currentblocksize);
      socket.emit('update', 'CurrentBlockTx: ' + getMiningInfo.currentblocktx);
-     socket.emit('update', 'Difficulty: ' + getMiningInfo.difficulty);
+     socket.emit('update', 'Difficulty: ' + numberWithCommas(getMiningInfo.difficulty.toFixed(2)));
      socket.emit('update', 'Block Priority Percentage: %' + getMiningInfo.blockprioritypercentage);
      socket.emit('update', 'Networkhashps: ' + getMiningInfo.networkhashps);
      socket.emit('update', 'PooledTx: ' + getMiningInfo.pooledtx);
@@ -233,7 +254,7 @@ io.on('connection', function(socket){
         
         var BIP21encoded = bitbox.BitcoinCash.encodeBIP21(address, options)
         // bitcoincash:qpum6dwnqmmysdggrprse8ccjq7ldcrfqgmmtgcmny?amount=12.5&label=coinbase%20donation&message=and%20ya%20don%27t%20stop
-        console.log(BIP21encoded);
+        //console.log(BIP21encoded);
         socket.emit('chat message', BIP21encoded);
 
       }
@@ -288,24 +309,25 @@ io.on('connection', function(socket){
     // debug: console.log('checking if msg qualifies for help module');
     if (msglow == 'help'){
       socket.emit('update', 'Enter a BCH address in Legacy or CashAddr format to display the latest info about that address. If an unconfirmed balance is detected for that address, the system will track it until it is confirmed.');
-      socket.emit('update', 'Enter "getBlockchainInfo" to return the latest info regarding the BCH Blockchain');
-      socket.emit('update', 'Enter "getBlockCount" to return the latest number of blocks in the longest Blockchain');
+      socket.emit('update', 'Enter "getBlockchainInfo" to return the latest info regarding the BCH Blockchain.');
+      socket.emit('update', 'Enter "getBlockCount" to return the latest number of blocks in the longest Blockchain.');
       socket.emit('update', 'Enter "getDifficulty" to return the proof-of-work difficulty as a multiple of the minimum difficulty.');
-      socket.emit('update', 'Enter "getMempoolInfo" to return the latest info about the current mempool');
-      socket.emit('update', 'Enter "toSatoshi(# of BCH)" to return the amount of Satoshis for the supplied amount of BCH');
-      socket.emit('update', 'Enter "toBitcoinCash(# of Satoshis)" to return the amount of BCH for supplied amount of Sats');
-      socket.emit('update', 'Enter "getMiningInfo" to return mining-related information');
-      socket.emit('update', 'Enter utxo(BCH_ADDRESS) to return a list of utxos for a legacy or cash address');
-      socket.emit('update', 'Enter "encryptBIP38, PRIVATEKEYWIF, PASSWORD" to encrypt privkey WIFs with BIP39');
-      socket.emit('example', 'encryptBIP38, L1phBREbhL4vb1uHHHCAse8bdGE5c7ic2PFjRxMawLzQCsiFVbvu, 9GKVkabAHBMyAf');
-      socket.emit('update', 'Enter "decryptBIP38, encryptedPRIVATEKEYWIF, PASSWORD to decrypt with, network"');
-      socket.emit('example', 'decryptBIP38, 6PYU2fDHRYfsKawL4KYornRFgimHnEovxU4VcgDaVtsVuuzQEfWDYwBQLd, Password123, mainnet');
+      socket.emit('update', 'Enter "getMempoolInfo" to return the latest info about the current mempool.');
+      socket.emit('update', 'Enter "toSatoshi(# of BCH)" to return the amount of Satoshis for the supplied amount of BCH.');
+      socket.emit('update', 'Enter "toBitcoinCash(# of Satoshis)" to return the amount of BCH for supplied amount of Sats.');
+      socket.emit('update', 'Enter "getMiningInfo" to return mining-related information.');
+      socket.emit('update', 'Enter utxo(BCH_ADDRESS) to return a list of utxos for a legacy or cash address.');
+      socket.emit('update', 'Enter "encryptBIP38, PRIVATEKEYWIF, PASSWORD" to encrypt privkey WIFs with BIP39.');
+      socket.emit('example', 'encryptBIP38, L1phBREbhL4vb1uHHHCAse8bdGE5c7ic2PFjRxMawLzQCsiFVbvu, 9GKVkabAHBMyAf.');
+      socket.emit('update', 'Enter "decryptBIP38, encryptedPRIVATEKEYWIF, PASSWORD" to decrypt with, network.');
+      socket.emit('example', 'decryptBIP38, 6PYU2fDHRYfsKawL4KYornRFgimHnEovxU4VcgDaVtsVuuzQEfWDYwBQLd, Password123, mainnet.');
       socket.emit('update', 'Enter "sign, PRIVATEKEYWIF, MESSAGE" to sign a message with a private key.');
       socket.emit('example', 'sign, KxtpRDUJDiutLaTV8Vuavhb6h7zq9YV9ZKA3dU79PCgYmNVmkkvS, Bitcoin Cash is Bitcoin');
       socket.emit('update', 'Enter "verify, BCH_ADDRESS, SIGNATURE, MESSAGE" to verify a signed message.');
       socket.emit('example', 'verify, bitcoincash:qp2zvw3zpk5xx43w4tve7mtekd9kaxwj4uenq9eupv, II+rsv72D/KnZ6noO25WWtXnu3C2seCuJEJ6HwB3yalFTbeBjF1FSw6XuaOnxpwsrEXHimIdJB9k3aKsJaQB0L0=, Bitcoin Cash is Bitcoin');
       socket.emit('update', 'Enter "donate" to view a donation address for KeepBitcoinFree.org');
-      socket.emit('update', 'Enter "reboot" or "clear" to clear the screen and start fresh');
+      socket.emit('update', 'Enter "reboot" or "clear" to reboot NodeBox, clear the screen and start fresh (not working on mobile).');
+      socket.emit('update', 'Enter "keepbitcoinfree.org" to visit our website & learn more.');
       return;
     }
   }catch(error){
@@ -322,10 +344,10 @@ try{
     let getMempoolInfo = await bitbox.Blockchain.getMempoolInfo();
    // console.log(getMempoolInfo);
    // print entire array at once: socket.emit('array', JSON.stringify(getMempoolInfo));
-    socket.emit('update', 'Size: ' + getMempoolInfo.size);
-    socket.emit('update', 'Bytes: ' + getMempoolInfo.bytes);
-    socket.emit('update', 'Usage: ' + getMempoolInfo.usage);
-    socket.emit('update', 'Max Mempool: ' + getMempoolInfo.maxmempool);
+    socket.emit('update', 'Size: ' + numberWithCommas(getMempoolInfo.size));
+    socket.emit('update', 'Bytes: ' + numberWithCommas(getMempoolInfo.bytes));
+    socket.emit('update', 'Usage: ' + numberWithCommas(getMempoolInfo.usage));
+    socket.emit('update', 'Max Mempool: ' + numberWithCommas(getMempoolInfo.maxmempool));
     socket.emit('update', 'Mempool Min Fee: ' + getMempoolInfo.mempoolminfee + ' BCH');
     socket.emit('update', 'Min Relay Tx Fee ' + getMempoolInfo.minrelaytxfee + ' BCH');
 
@@ -348,8 +370,8 @@ try{
     (async () => {
   try {
     let getBlockCount = await bitbox.Blockchain.getBlockCount();
-    console.log(getBlockCount);
-    socket.emit('update', 'Block Count: ' + getBlockCount);
+    //console.log(getBlockCount);
+    socket.emit('update', 'Bitcoin Cash Block Count: ' + numberWithCommas(getBlockCount));
   } catch(error) {
    console.error(error)
   }
@@ -370,7 +392,7 @@ try{
   try {
     let getDifficulty = await bitbox.Blockchain.getDifficulty();
     // debug: console.log(getDifficulty);
-    socket.emit('update', 'Difficulty: ' + getDifficulty);
+    socket.emit('update', 'Difficulty: ' + numberWithCommas(getDifficulty.toFixed(2)));
   } catch(error) {
    console.error(error)
   }
@@ -394,10 +416,10 @@ try{
     //console.log(getBlockchainInfo);
     //socket.emit('update', 'BlockChain Info for BCH:');
     socket.emit('update', 'Chain: ' + getBlockchainInfo.chain);
-    socket.emit('update', 'Blocks: ' + getBlockchainInfo.blocks);
-    socket.emit('update', 'Headers: ' + getBlockchainInfo.headers);
+    socket.emit('update', 'Blocks: ' + numberWithCommas(getBlockchainInfo.blocks));
+    socket.emit('update', 'Headers: ' + numberWithCommas(getBlockchainInfo.headers));
     socket.emit('update', 'Best Block Hash: ' + getBlockchainInfo.bestblockhash);
-    socket.emit('update', 'Difficulty: ' + getBlockchainInfo.difficulty);
+    socket.emit('update', 'Difficulty: ' + numberWithCommas(getBlockchainInfo.difficulty.toFixed(2)));
     socket.emit('update', 'Mediantime: ' + getBlockchainInfo.mediantime);
     socket.emit('update', 'Verification Progress: ' + getBlockchainInfo.verificationprogress);
     socket.emit('update', 'Chainwork: ' + getBlockchainInfo.chainwork);
@@ -421,54 +443,83 @@ try{
   
   try {
    
-   //TODO: move price grab outside of address balance details to use elsewhere in price(amount of BCH)
+   //TODO: move price grab outside of address balance details to use elsewhere in price(amount of BCH) ie. create a function for pricegrab.
   (async () => {
     // use BITBOX to get details of BCH address submitted by user
     let details = await bitbox.Address.details(msg);
     // use BITBOX to GET BCH PRICE IN USD
     let usd = await bitbox.Price.current('usd');
     usd = usd / 100;
+    
     let balanceUsd = details.balance * usd;
-    // break down details array & print to user
-    socket.emit('update', 'Balance: ' + details.balance + ' BCH  ($' + balanceUsd.toFixed(2) + ' USD)');
-    socket.emit('update', 'Balance in Sats: ' + details.balanceSat);
-    socket.emit('update', 'Unconfirmed Balance: ' + details.unconfirmedBalance + ' BCH');
-    socket.emit('update', 'Unconfirmed Balance in Sats: ' + details.unconfirmedBalanceSat);
-    socket.emit('update', 'Total Received: ' + details.totalReceived + ' BCH');
-    socket.emit('update', 'Total Sats Received: ' + details.totalReceivedSat);
-	  socket.emit('update', 'Total Sats Sent: ' + details.totalSentSat);
-   
-	if (details.unconfirmedBalance > 0){
-    socket.emit('update', 'Unconfirmed Balance: ' + details.unconfirmedBalance + ' BCH');
+    let totalRec = numberWithCommas((details.totalReceived * usd).toFixed(2));
 
+    //if $ is over 3 digits, add a comma  = $420,420
+    balanceUsd = numberWithCommas(balanceUsd.toFixed(2));
+
+    // break down details array & print to user
+    socket.emit('update', 'Balance: ' + details.balance + ' BCH  ($' + balanceUsd + ' USD)');
+    socket.emit('update', 'Balance in Sats: ' + numberWithCommas(details.balanceSat));
+    socket.emit('update', '*Unconfirmed Balance: ' + details.unconfirmedBalance + ' BCH');
+    socket.emit('update', 'Unconfirmed Balance in Sats: ' + numberWithCommas(details.unconfirmedBalanceSat));
+    socket.emit('update', 'Total Received: ' + details.totalReceived + ' BCH ($' + totalRec + ' USD)');
+    socket.emit('update', 'Total Sats Received: ' + numberWithCommas(details.totalReceivedSat));
+	  socket.emit('update', 'Total Sent: ' + bitbox.BitcoinCash.toBitcoinCash(details.totalSentSat) + 'BCH');
+    socket.emit('update', 'Total Sats Sent: ' + numberWithCommas(details.totalSentSat));
+   
+   //unconfirmed balance exists, tell user we're tracking it in the background
+	if (details.unconfirmedBalance !== 0){
+    socket.emit('example', '*Unconfirmed Balance detected: ' + details.unconfirmedBalance + ' BCH. NodeBox is tracking the balance. You can continue working & will be notified when it is confirmed.');
   }
 
-  while (details.unconfirmedBalance > 0){
+  //let i = 0;
+
+  //UNCONFIRMED BALANCE LOOP 
+  //unconfirmed balance detected. Let's track it and let the user know when it confirms.
+  while (details.unconfirmedBalance !== 0){
 			//socket.emit('update', 'Unconfirmed balance detected.... Tracking the balance until it is confirmed.')
+
+      //TODO: add a var on client side to update while it's checking for a confirmation. something like a waiting ........
+     // while (i < 5){
+        //socket.emit('waiting', '.');
+        //console.log('i = ' + i);
+        //do something. i'd like to make a progress bar of sorts but it might be better to just let the user continue without having to wait.
+        // need to test if app will continue tracking while user continues issuing commands.
+       // i++;
+      
+     // if(i == 5){
+     //   i == 0;
+     // }
 
       //re-check the balance every 10 seconds until balance == 0
 			await new Promise(resolve => setTimeout(resolve, 10000));
 
 			let details = await bitbox.Address.details(msg);
 
-      // check if balance is 0
+      // each pass while unconfirmedBalance > 0, check if balance is 0 (has confirmed) & alert user if so.
 			if (details.unconfirmedBalance == 0){
 				socket.emit('chat message', 'Unconfirmed Balance for ' + msg + ' has been confirmed!');
-				// TODO: alert user funds have confirmed (needs to be done on client side): window.alert('Unconfirmed Balance has been confirmed!');
+
+      //socket.emit('logo', '_╔╗_┌─┐┬__┌─┐┌┐┌┌─┐┌─┐__┬_┬┌─┐┌─┐__┌┐_┌─┐┌─┐┌┐┌__╔═╗╔═╗╔╗╔╔═╗╦╦═╗╔╦╗╔═╗╔╦╗┬');
+      //socket.emit('logo', '_╠╩╗├─┤│__├─┤││││__├┤___├─┤├─┤└─┐__├┴┐├┤_├┤_│││__║__║_║║║║╠╣_║╠╦╝║║║║╣__║║│');
+      //socket.emit('logo', '_╚═╝┴_┴┴─┘┴_┴┘└┘└─┘└─┘__┴_┴┴_┴└─┘__└─┘└─┘└─┘┘└┘__╚═╝╚═╝╝╚╝╚__╩╩╚═╩_╩╚═╝═╩╝o');
+
+
+        // TODO: real deal alert user funds have confirmed (needs to be done on client side): window.alert('Unconfirmed Balance has been confirmed!');
 				return;
-			}
+			//}
 
 		}
+
+  }
     //separator for each address
     //socket.emit('chat message', '');
-  })()
+   })()
 
   } catch(error) {
   // catch error for details array and print only to console if it fails
    console.error(error)
   }
-
-
     
   // bitcoincash:qzm47qz5ue99y9yl4aca7jnz7dwgdenl85jkfx3znl
 
@@ -476,14 +527,15 @@ try{
   
   //CONVERT BCH ADDRESS: detects address type & converts to the other, prints both.
   try{
-    	if ( bitbox.Address.isLegacyAddress(msg) == true) {
 
+    	if ( bitbox.Address.isLegacyAddress(msg) == true) {
     		//print original address
     		socket.emit('update', 'Legacy: ' + msg);
-
     		// mainnet w/ prefix
   			cashAddr = bitbox.Address.toCashAddress(msg);
+        // get/set the details before we continue with output
         network = bitbox.Address.detectAddressNetwork(msg);
+        
   			// bitcoincash:qzm47qz5ue99y9yl4aca7jnz7dwgdenl85jkfx3znl
   			socket.emit('update', 'CashAddr: ' + cashAddr);
   			socket.emit('update', 'Network: ' + network);
@@ -492,13 +544,15 @@ try{
     	
     	 if (bitbox.Address.isCashAddress(msg) == true) {
 
-    	  	// print original address
-    	   socket.emit('update', 'CashAddress: ' + msg);
-
-    		// mainnet w/ prefix
-  			legacy = bitbox.Address.toLegacyAddress(msg);
+        // mainnet w/ prefix
+        legacy = bitbox.Address.toLegacyAddress(msg);
+        // mainnet w/ prefix
+        cashAddr = bitbox.Address.toCashAddress(msg);
+        // get/set the details before we continue with output
         network = bitbox.Address.detectAddressNetwork(msg);
-  			// 1HiaTupadqQN66Tvgt7QSE5Wg13BUy25eN
+
+    	  // print addresses
+        socket.emit('update', 'CashAddr: ' + cashAddr);
   			socket.emit('update', 'Legacy: ' + legacy);
   			socket.emit('update', 'Network: ' + network);
   			return;
